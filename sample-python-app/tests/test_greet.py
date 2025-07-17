a22dd5daa58a9572676932fd486c138c67b5f654
hashanwijeratne92 <hashanwijeratne92@gmail.com>
2025-07-17 22:54:17 +0400

    test1

--- sample-python-app/tests/test_greet.py
+++ sample-python-app/tests/test_greet.py
@@ -2,4 +2,4 @@ from hello_pipeline import greet
 
 
 def test_greet_default():
-    assert greet() == "Hello, Azure DevOps! This build ran successfully."
+    assert greet() == "Hello, Azure DevOps! This build ran successfully. test"
